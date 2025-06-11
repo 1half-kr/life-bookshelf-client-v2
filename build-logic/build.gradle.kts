4256f461955697ea14fa3a0be2549a2d349d5cd0
emjayMJkim <minjeong030420@gmail.com>
2025-06-11 16:27:40 +0900

    [feat] Set each module gradle plugin
    Set build gradle plugin in each module
    
    Related to: #1

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,9 +1,56 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    `kotlin-dsl`
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation(libs.agp)
+    implementation(libs.kotlin.gradleplugin)
+    compileOnly(libs.compose.compiler.extension)
+}
+
+gradlePlugin {
+    plugins {
+        register("android-application") {
+            id = "tdd.android.application"
+            implementationClass = "com.tdd.plugin.AndroidApplicationPlugin"
+        }
+        register("android-compose") {
+            id = "tdd.android.compose"
+            implementationClass = "com.tdd.plugin.AndroidComposePlugin"
+        }
+        register("android-feature") {
+            id = "tdd.android.feature"
+            implementationClass = "com.tdd.plugin.AndroidFeaturePlugin"
+        }
+        register("android-hilt") {
+            id = "tdd.android.hilt"
+            implementationClass = "com.tdd.plugin.AndroidHiltPlugin"
+        }
+        register("android-kotlin") {
+            id = "tdd.android.kotlin"
+            implementationClass = "com.tdd.plugin.AndroidKotlinPlugin"
+        }
+        register("kotlin-jvm") {
+            id = "tdd.kotlin.jvm"
+            implementationClass = "com.tdd.plugin.KotlinJvmPlugin"
+        }
+        register("kotlin-serialization") {
+            id = "tdd.kotlin.serialization"
+            implementationClass = "com.tdd.plugin.KotlinSerializationPlugin"
+        }
+        register("retrofit") {
+            id = "tdd.retrofit"
+            implementationClass = "com.tdd.plugin.RetrofitPlugin"
+        }
+    }
 }
