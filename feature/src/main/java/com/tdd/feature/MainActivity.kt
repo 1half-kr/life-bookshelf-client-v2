759837698783b603ad6e255c644239a85b27dfdf
emjayMJkim <minjeong030420@gmail.com>
2025-06-11 16:33:36 +0900

    [feat] Set application and mainActivity
    Set application and Refactor mainActivity
    Delete unused file
    
    Related to: #1

--- feature/src/main/java/com/tdd/feature/MainActivity.kt
+++ feature/src/main/java/com/tdd/feature/MainActivity.kt
@@ -1,31 +1,20 @@
-package com.tdd.bookshelf
+package com.tdd.feature
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.tdd.bookshelf.ui.theme.BookshelfTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BookshelfTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Greeting(name = "인생사전")
         }
     }
 }
@@ -41,7 +30,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    BookshelfTheme {
-        Greeting("Android")
-    }
+    Greeting("")
 }
