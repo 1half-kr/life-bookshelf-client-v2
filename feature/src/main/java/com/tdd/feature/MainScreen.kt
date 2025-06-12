bfdc79bc66990268e72a3c196962bff1546be4d6
emjayMJkim <minjeong030420@gmail.com>
2025-06-12 18:45:25 +0900

    [feat] Set main screen and activity
    Set MainScreen and MainActivity
    
    Related to: #3

---
+++ feature/src/main/java/com/tdd/feature/MainScreen.kt
@@ -0,0 +1,32 @@
+package com.tdd.feature
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.tdd.navigation.NavRoutes
+import com.tdd.navigation.onBoardingNavGraph
+
+@Composable
+fun MainScreen() {
+
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = { BottomNavBar() }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = NavRoutes.OnBoardingGraph.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            onBoardingNavGraph(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun BottomNavBar() {
+}
